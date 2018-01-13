96eccb0b7dfc1f12e7f4450054b20e72299ab178
Thomas Alexander <thomasalexander2718@gmail.com>
2018-01-12 21:13:20 -0500

    Reverted switch to resampler

--- src/qinfer/abstract_model.py
+++ src/qinfer/abstract_model.py
@@ -622,11 +622,10 @@ class Model(Simulatable):
 
             outcomes = self.simulate_experiment(modelparams, expparams, repeat=1)[0,:,:]
         
-
+            np.unique(self.simulate_experiment(modelparams, expparams, repeat=1)[0,:,:],axis=0).shape
             if not self.allow_identical_outcomes:
                 outcomes = np.unique(outcomes,axis=0)
 
-           
             L = self.likelihood(outcomes, likelihood_modelparams, expparams).transpose(2,0,1)
 
 
